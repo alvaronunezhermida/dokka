44b0f36ab900c2a00a917747113044a260216472
Szymon Świstun <sswistun@virtuslab.com>
2020-01-08 17:36:06 +0100

    enums added

--- core/src/main/kotlin/model/Documentable.kt
+++ core/src/main/kotlin/model/Documentable.kt
@@ -1,9 +1,10 @@
 package org.jetbrains.dokka.model
 
-import org.jetbrains.dokka.model.doc.*
-import org.jetbrains.dokka.transformers.descriptors.KotlinTypeWrapper
 import org.jetbrains.dokka.links.DRI
+import org.jetbrains.dokka.model.doc.DocumentationNode
 import org.jetbrains.dokka.pages.PlatformData
+import org.jetbrains.dokka.transformers.descriptors.KotlinClassKindTypes
+import org.jetbrains.dokka.transformers.descriptors.KotlinTypeWrapper
 
 class Module(override val name: String, val packages: List<Package>) : Documentable() {
     override val dri: DRI = DRI.topLevel
@@ -12,52 +13,115 @@ class Module(override val name: String, val packages: List<Package>) : Documenta
 }
 
 class Package(
-    override val dri: DRI,
-    override val functions: List<Function>,
-    override val properties: List<Property>,
-    override val classes: List<Class>,
-    override val extra: MutableSet<Extra> = mutableSetOf()
+        override val dri: DRI,
+        override val functions: List<Function>,
+        override val properties: List<Property>,
+        override val classlikes: List<Classlike>,
+        override val extra: MutableSet<Extra> = mutableSetOf()
 ) : ScopeNode() {
     override val name = dri.packageName.orEmpty()
 }
 
 class Class(
-    override val dri: DRI,
-    override val name: String,
-    val kind: ClassKind,
-    val constructors: List<Function>,
-    override val functions: List<Function>,
-    override val properties: List<Property>,
-    override val classes: List<Class>,
-    override val expected: ClassPlatformInfo?,
-    override val actual: List<ClassPlatformInfo>,
-    override val extra: MutableSet<Extra> = mutableSetOf()
-) : ScopeNode() {
-    val inherited by lazy { platformInfo.mapNotNull { (it as? ClassPlatformInfo)?.inherited }.flatten() }
+        override val dri: DRI,
+        override val name: String,
+        override val kind: ClassKind,
+        val constructors: List<Function>,
+        override val functions: List<Function>,
+        override val properties: List<Property>,
+        override val classlikes: List<Classlike>,
+        override val expected: ClassPlatformInfo?,
+        override val actual: List<ClassPlatformInfo>,
+        override val extra: MutableSet<Extra> = mutableSetOf()
+) : Classlike(
+        name = name,
+        dri = dri,
+        kind = kind,
+        functions = functions,
+        properties = properties,
+        classlikes = classlikes,
+        expected = expected,
+        actual = actual,
+        extra = extra
+)
+
+class Enum(
+        override val dri: DRI,
+        override val name: String,
+        val entries: List<EnumEntry>,
+        val constructors: List<Function>,
+        override val functions: List<Function> = emptyList(),
+        override val properties: List<Property> = emptyList(),
+        override val classlikes: List<Classlike> = emptyList(),
+        override val expected: ClassPlatformInfo? = null,
+        override val actual: List<ClassPlatformInfo>,
+        override val extra: MutableSet<Extra> = mutableSetOf()
+) : Classlike(dri = dri, name = name, kind = KotlinClassKindTypes.ENUM_CLASS, actual = actual) {
+    constructor(c: Classlike, entries: List<EnumEntry>, ctors: List<Function>) : this(
+            dri = c.dri,
+            name = c.name,
+            entries = entries,
+            constructors = ctors,
+            functions = c.functions,
+            properties = c.properties,
+            classlikes = c.classlikes,
+            expected = c.expected,
+            actual = c.actual,
+            extra = c.extra
+    )
+
+    override val children: List<Documentable>
+        get() = entries
+}
+
+class EnumEntry(
+        override val dri: DRI,
+        override val name: String,
+        override val expected: ClassPlatformInfo? = null,
+        override val actual: List<ClassPlatformInfo>,
+        override val extra: MutableSet<Extra> = mutableSetOf()
+) : Classlike(
+        dri = dri,
+        name = name,
+        actual = actual,
+        expected = expected,
+        extra = extra,
+        kind = KotlinClassKindTypes.ENUM_ENTRY
+) {
+    constructor(c: Classlike) : this(
+            dri = c.dri,
+            name = c.name,
+            actual = c.actual,
+            expected = c.expected,
+            extra = c.extra
+    )
+
+    override val children: List<Parameter>
+        get() = emptyList()
 }
 
 class Function(
-    override val dri: DRI,
-    override val name: String,
-    val returnType: TypeWrapper?,
-    val isConstructor: Boolean,
-    override val receiver: Parameter?,
-    val parameters: List<Parameter>,
-    override val expected: PlatformInfo?,
-    override val actual: List<PlatformInfo>,
-    override val extra: MutableSet<Extra> = mutableSetOf()
+        override val dri: DRI,
+        override val name: String,
+        val returnType: TypeWrapper?,
+        val isConstructor: Boolean,
+        override val receiver: Parameter?,
+        val parameters: List<Parameter>,
+        override val expected: PlatformInfo?,
+        override val actual: List<PlatformInfo>,
+        override val extra: MutableSet<Extra> = mutableSetOf()
 ) : CallableNode() {
     override val children: List<Parameter>
         get() = listOfNotNull(receiver) + parameters
 }
 
 class Property(
-    override val dri: DRI,
-    override val name: String,
-    override val receiver: Parameter?,
-    override val expected: PlatformInfo?,
-    override val actual: List<PlatformInfo>,
-    override val extra: MutableSet<Extra> = mutableSetOf()
+        override val dri: DRI,
+        override val name: String,
+        override val receiver: Parameter?,
+        override val expected: PlatformInfo?,
+        override val actual: List<PlatformInfo>,
+        override val extra: MutableSet<Extra> = mutableSetOf()
 ) : CallableNode() {
     override val children: List<Parameter>
         get() = listOfNotNull(receiver)
@@ -65,11 +129,11 @@ class Property(
 
 // TODO: treat named Parameters and receivers differently
 class Parameter(
-    override val dri: DRI,
-    override val name: String?,
-    val type: TypeWrapper,
-    override val actual: List<PlatformInfo>,
-    override val extra: MutableSet<Extra> = mutableSetOf()
+        override val dri: DRI,
+        override val name: String?,
+        val type: TypeWrapper,
+        override val actual: List<PlatformInfo>,
+        override val extra: MutableSet<Extra> = mutableSetOf()
 ) : Documentable() {
     override val children: List<Documentable>
         get() = emptyList()
@@ -81,19 +145,19 @@ interface PlatformInfo {
 }
 
 class BasePlatformInfo(
-    override val documentationNode: DocumentationNode,
-    override val platformData: List<PlatformData>) : PlatformInfo {
+        override val documentationNode: DocumentationNode,
+        override val platformData: List<PlatformData>) : PlatformInfo {
 
     override fun equals(other: Any?): Boolean =
-        other is PlatformInfo && documentationNode == other.documentationNode
+            other is PlatformInfo && documentationNode == other.documentationNode
 
     override fun hashCode(): Int =
-        documentationNode.hashCode()
+            documentationNode.hashCode()
 }
 
 class ClassPlatformInfo(
-    val info: PlatformInfo,
-    val inherited: List<DRI>) : PlatformInfo by info
+        val info: PlatformInfo,
+        val inherited: List<DRI>) : PlatformInfo by info
 
 abstract class Documentable {
     open val expected: PlatformInfo? = null
@@ -114,31 +178,44 @@ abstract class Documentable {
     override fun hashCode() = dri.hashCode()
 
 
-
     val commentsData: List<DocumentationNode>
         get() = platformInfo.map { it.documentationNode }
 
     val briefDocTagString: String
         get() =
             platformInfo
-            .firstOrNull()
-            ?.documentationNode
-            ?.children
-            ?.firstOrNull()
-            ?.root
-            ?.docTagSummary()
-            ?.shorten(40) ?: ""
+                    .firstOrNull()
+                    ?.documentationNode
+                    ?.children
+                    ?.firstOrNull()
+                    ?.root
+                    ?.docTagSummary()
+                    ?.shorten(40) ?: ""
 
     open val extra: MutableSet<Extra> = mutableSetOf()
+}
+
+abstract class Classlike(
+        override val dri: DRI,
+        override val name: String,
+        open val kind: ClassKind,
+        override val functions: List<Function> = emptyList(),
+        override val properties: List<Property> = emptyList(),
+        override val classlikes: List<Classlike> = emptyList(),
+        override val expected: ClassPlatformInfo? = null,
+        override val actual: List<ClassPlatformInfo>,
+        override val extra: MutableSet<Extra> = mutableSetOf()
+) : ScopeNode() {
+    val inherited by lazy { platformInfo.mapNotNull { (it as? ClassPlatformInfo)?.inherited }.flatten() }
 }
 
 abstract class ScopeNode : Documentable() {
     abstract val functions: List<Function>
     abstract val properties: List<Property>
-    abstract val classes: List<Class>
+    abstract val classlikes: List<Classlike>
 
     override val children: List<Documentable>
-        get() = functions + properties + classes
+        get() = functions + properties + classlikes
 }
 
 abstract class CallableNode : Documentable() {
@@ -155,13 +232,14 @@ interface TypeWrapper {
     val arguments: List<KotlinTypeWrapper>
     val dri: DRI?
 }
+
 interface ClassKind
 
 fun Documentable.dfs(predicate: (Documentable) -> Boolean): Documentable? =
-    if (predicate(this)) {
-        this
-    } else {
-        this.children.asSequence().mapNotNull { it.dfs(predicate) }.firstOrNull()
-    }
+        if (predicate(this)) {
+            this
+        } else {
+            this.children.asSequence().mapNotNull { it.dfs(predicate) }.firstOrNull()
+        }
 
 interface Extra
